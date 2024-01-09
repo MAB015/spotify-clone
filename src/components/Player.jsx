2f82bc2168ca37e831ab497eeec3c695c665a3cf
Miguel Ángel <MAB015@HOTMAIL.COM>
2024-01-09 05:47:15 -0500

    Formating time, and adding Current Time bar with the interaction

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import { usePlayerStore } from "@/store/playerStore"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Slider } from "@/components/Slider"
 
 export const Pause = () => (
@@ -51,8 +51,55 @@ const CurrentSong = ({ image, title, artists }) => {
     )
 }
 
-const songControl = ( audio ) => {
+const SongControl = ( { audio } ) => {
+    const [currentTime, setCurrentTime] = useState(0)
+
+    useEffect(() => {
+        audio.current.addEventListener('timeupdate', handleTimeUpdate)
     
+        return () => {
+            audio.current.removeEventListener('timeupdate', handleTimeUpdate)
+        }
+    }, [])
+
+    const handleTimeUpdate = () => {
+        setCurrentTime(audio.current.currentTime)
+    }
+
+    const formatTime = (time) => {
+        if (time == null) return `0:00`
+
+        const seconds = Math.floor( time % 60 )
+        const minutes = Math.floor( time / 60 )
+
+        return `${minutes}:${seconds.toString().padStart(2, '0')}`
+    }
+
+    const duration = audio?.current?.duration ?? 0
+
+    return (
+        <div className="flex gap-x-3 text-xs pt-2">
+            <span className="opacity-50 w-12 text-right">
+                { formatTime(currentTime) }
+            </span>
+
+            <Slider
+                defaultValue={[0]}
+                value = {[currentTime]}
+                max={audio?.current?.duration ?? 0}
+                min={0}
+                className="w-[400px]"
+                onValueChange={(value) => {
+                    const [newCurrentTime] = value
+                    audio.current.currentTime = newCurrentTime
+                }}
+            />
+
+            <span className="opacity-50 w-12">
+                {duration ? formatTime(duration) : '0:00' }
+            </span>
+        </div>
+    )
 }
 
 const VolumeControl = () => {
@@ -137,13 +184,13 @@ export function Player () {
     }
 
     return (
-        <div className="flex flex-row justify-between w-full px-4 z-50">
-            <div>
+        <div className="flex flex-row justify-between w-full px-1 z-50">
+            <div className="w-[250px]">
                 <CurrentSong {... currentMusic.song } />
             </div>
 
             <div className="grid place-content-center gap-4 flex-1">
-                <div>
+                <div className="flex flex-col justify-center items-center">
                     <button
                         className="bg-white rounded-full p-2"
                         onClick={handleClick}
@@ -152,6 +199,7 @@ export function Player () {
                             isPlaying ? <Pause /> : <Play />
                         }
                     </button>
+                    <SongControl audio={ audioRef } />
                     <audio ref={audioRef} />
                 </div>
             </div>
