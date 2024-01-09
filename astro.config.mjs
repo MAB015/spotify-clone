1388a1ab59650438f12f78d498040068dadfcf99
Miguel Ángel <MAB015@HOTMAIL.COM>
2024-01-08 22:04:36 -0500

    - Fixing Backgrounds - Organizing Layout - Adding component with svelte - Adding component with reactjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte(), react()],
   output: 'server',
   adapter: netlify()
 });
---
+++ src/components/Player.jsx
@@ -0,0 +1,25 @@
+const Pause = () => (
+    ss
+)
+
+const Play = () => {
+    ss
+}
+
+export function Player () {
+    return (
+        <div className="flex flex-row justify-between w-full px-4 z-50">
+            <div>
+                CurrentSong...
+            </div>
+
+            <div className="">
+                Reproductor...
+            </div>
+
+            <div className="">
+                Volumen...
+            </div>
+        </div>
+    )
+}
